c7ff0742cd3284db59e069408bc3fee6038a13c7
boydjohnson <johnson.boyd@gmail.com>
2015-11-19 18:18:24 -0600

    Created base.html that all others will extend. Validation of model Chatroom, fixed view to only query Chatrooms if the user.is_authenticated

--- viewer/models.py
+++ viewer/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 import uuid
+
+
+def only_two_chatrooms_per_user(obj):
+    model = obj.__class__
+    if model.objects.filter(owner=obj.owner) >= 2:
+        raise ValidationError("Can only create 2 %s instances per user" % model.__name__)
+
+
 
 
 class ChatRoom(models.Model):
@@ -11,9 +20,13 @@ class ChatRoom(models.Model):
     uuid = models.UUIDField(verbose_name='chatroom_uuid', null=False, default=uuid.uuid4, unique=True)
     owner = models.ForeignKey(User, related_name='chatroom_from_owner', null=False)
     users = models.ManyToManyField(User, related_name='chatroom_from_users', null=False)
-    type = models.IntegerField(editable=False, null=False)
+
 
 
     def __unicode__(self):
         return self.name + " " + self.owner.username
 
+
+    def clean(self):
+        only_two_chatrooms_per_user(self)
+        super(self, ChatRoom).clean()
--- viewer/views.py
+++ viewer/views.py
@@ -3,17 +3,28 @@ from django.template.context import RequestContext
 
 from django.contrib.auth.views import logout as auth_logout
 
+from viewer.models import ChatRoom
+
 # Create your views here.
 
 
 def login(request):
     if request.method == 'GET':
-        context = RequestContext(request, {'user': request.user})
+        user = request.user
+        if user.is_authenticated():
+            chatrooms = ChatRoom.objects.filter(users=user)
+        else:
+            chatrooms = []
+        number_of_owned_chatrooms = 0
+        for c in chatrooms:
+            if c.owner == user:
+                number_of_owned_chatrooms += 1
+        context = RequestContext(request, {'user': user, 'number_owned': number_of_owned_chatrooms, 'chatrooms': chatrooms})
         return render(request, 'login.html', context=context)
-
 
 
 def logout(request):
     """Logs out user"""
     auth_logout(request)
     return redirect('/')
+
