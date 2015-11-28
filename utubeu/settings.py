912d0ab66cd0f50e18a3b6c278d1e824f34ce994
boydjohnson <johnson.boyd@gmail.com>
2015-11-28 01:58:34 -0600

    Form is working but not creating correctly Chatroom Model instances

--- utubeu/settings.py
+++ utubeu/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),
+               )
 
 AUTHENTICATION_BACKENDS = [
     'social.backends.open_id.OpenIdAuth',
