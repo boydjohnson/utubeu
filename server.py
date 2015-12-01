fe9845d1d532a286e8010b4a29e0da26a7975403
boydjohnson <lt359mn38>
2015-12-01 14:16:55 -0600

    Working nominally

--- server.py
+++ server.py
@@ -1,54 +1,20 @@
-from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol, listenWS
+from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol
 
 from twisted.internet import reactor
 from json import dumps
 
 
-SCREENS = ['Firefox', 'Chrome']
-PHONES = ['Android']
-
-
-
-def determineUser(text):
-    if "Firefox" in text:
-        return 'Firefox'
-    elif "Chrome" in text:
-        return "Chrome"
-    else:
-        return "Other"
-
-
-
 class YouTubeWebSockets(WebSocketServerProtocol):
 
     def onConnect(self, request):
-        device = determineUser(request.headers[u'user-agent'])
-        if device in PHONES:
-            self.factory.phones.append(self)
-        elif device in SCREENS:
-            self.factory.screens.append(self)
-            for s in self.factory.screens:
-                s.sendMessage(dumps({'screen': {'count':len(self.factory.screens)}}).encode('utf-8'))
-        else:
-            self.factory.other.append(self)
-
-    def onClose(self, wasClean, code, reason):
-        device = determineUser(self.http_headers[u'user-agent'])
-        if device in PHONES:
-            self.factory.phones.remove(self)
-        elif device in SCREENS:
-            self.factory.screens.remove(self)
-            for s in self.factory.screens:
-                s.sendMessage(dumps({'screen': {'count':len(self.factory.screens)}}).encode('utf-8'))
-        else:
-            self.factory.other.remove(self)
+        self.factory.users.append(self)
 
     def onMessage(self, payload, isBinary):
-        JSON = {'screen':{'count':len(self.factory.screens)}}
-        print len(self.factory.screens)
+        JSON = {'screen':{'count':len(self.factory.users)}}
+        print len(self.factory.users)
         if not isBinary:
             JSON['msg'] = payload.decode('utf-8')
-            for s in self.factory.screens:
+            for s in self.factory.users:
                 s.sendMessage(dumps(JSON).encode('utf-8'))
 
 
@@ -57,9 +23,7 @@ class SeparateServerFactory(WebSocketServerFactory):
 
     def __init__(self, url, debug=False, debugCodePaths=False):
         super(SeparateServerFactory, self).__init__(url, debug, debugCodePaths)
-        self.phones = []
-        self.screens = []
-        self.other = []
+        self.users = []
 
 
 
@@ -68,8 +32,6 @@ if __name__ == '__main__':
 
     factory = SeparateServerFactory(u"ws://127.0.0.1:9000", debug=False)
     factory.protocol = YouTubeWebSockets
-
-
 
     reactor.listenTCP(9000, factory)
     reactor.run()
