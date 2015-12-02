4ff795a7441f47ddd21735c6f5df2e388e178e2d
boydjohnson <johnson.boyd@gmail.com>
2015-12-01 20:44:17 -0600

    Getting postgres url right

--- utubeu/settings.py
+++ utubeu/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 try:
     from socialConfig import google_client_id, google_secret
 except:
@@ -84,12 +85,9 @@ WSGI_APPLICATION = 'utubeu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DATABASE_URL'],
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+
 
 
 # Internationalization
