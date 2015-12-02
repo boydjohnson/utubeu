f3439f91dcc0f9128cfa19732bebcc73f31ddccd
boydjohnson <johnson.boyd@gmail.com>
2015-12-01 20:59:47 -0600

    Screwup again with urls

--- utubeu/urls.py
+++ utubeu/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'admin', include(admin.site.urls)),
-    url(r'social', include('social.apps.django_app.urls',namespace='social')),
+    url(r'developAdmin/', include(admin.site.urls)),
+    url(r'social/', include('social.apps.django_app.urls',namespace='social')),
     url(r'', include('viewer.urls', namespace='viewer')),
 ]
