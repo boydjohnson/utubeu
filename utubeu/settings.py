b7b7de216fb16c86ed628d50270de98a9cba6c08
boydjohnson <johnson.boyd@gmail.com>
2015-12-01 20:32:16 -0600

    Will get it right

--- utubeu/settings.py
+++ utubeu/settings.py
@@ -112,7 +112,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'statics'),
-                    os.path.join(BASE_DIR, 'collection')
                    )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'collection')
