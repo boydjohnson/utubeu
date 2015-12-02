15c0538f4f0904c0af04dc8d24ab049c54e948da
boydjohnson <johnson.boyd@gmail.com>
2015-12-01 22:22:23 -0600

    debugging

--- viewer/forms.py
+++ viewer/forms.py
@@ -8,9 +8,6 @@ import sys
 class EmailMultiField(forms.MultiValueField):
     def __init__(self, *args, **kwargs):
         fields = (forms.EmailField() for f in kwargs.pop('user_emails', []))
-        print "EMAILS IN EMAILMULTIFIELD", fields
-        sys.stdout.flush()
-
         super(EmailMultiField, self).__init__(fields, *args, **kwargs)
 
     def compress(self, data_list):
@@ -26,8 +23,6 @@ class Chatroom_with_InvitedChatroom(forms.Form):
     def __init__(self, *args, **kwargs):
         super(Chatroom_with_InvitedChatroom, self).__init__(*args, **kwargs)
         emails = args[0].pop('user_emails') if 'user_emails' in args[0].keys() else []
-        print "EMAILS all around ******", emails
-        sys.stdout.flush()
         self.fields['user_emails'] = EmailMultiField(user_emails = emails)
 
 
