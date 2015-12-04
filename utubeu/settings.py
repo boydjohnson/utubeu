96fae1daa93e22879dd2d415c1f2736f1d97fb28
boydjohnson <lt359mn38>
2015-12-03 19:18:10 -0600

    Make where getting database flexible through try: except:

--- utubeu/settings.py
+++ utubeu/settings.py
@@ -92,14 +92,15 @@ WSGI_APPLICATION = 'utubeu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config()
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+try:
+    DATABASES = {}
+    DATABASES['default'] = dj_database_url.config()
+except:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
 }
 
 
