fb47ef1a978054164d2ad27abb1b0593af90d2ae
boydjohnson <lt359mn38>
2015-12-06 18:50:51 -0600

    Close to done with REST api, debugging.

--- utubeuAPI/serializers.py
+++ utubeuAPI/serializers.py
@@ -7,13 +7,23 @@ class ChatroomSerializer(serializers.ModelSerializer):
         model = Chatroom
         fields = ('id', 'name', 'description')
 
+
 class ChatroomDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chatroom
         fields = ('id', 'name', 'description', 'users')
 
+    def update(self, instance, validated_data):
+        new_user = validated_data.get('users')
+        instance.users.add(new_user)
+        instance.save()
+        return instance
 
-class InvitedEmails(serializers.ModelSerializer):
+
+class InvitedEmailsSerializer(serializers.ModelSerializer):
+
+
+
     class Meta:
         model = InvitedEmails
         fields = '__all__'
