6478482ed905418491a48848895765668b1ec13d
boydjohnson <johnson.boyd@gmail.com>
2015-12-07 22:15:34 -0600

    Working on API urls

--- utubeuAPI/urls.py
+++ utubeuAPI/urls.py
@@ -6,5 +6,6 @@ from utubeuAPI.views import ChatroomDetailView, OwnedChatroomListCreateView, Joi
 urlpatterns = [
         url(r'^ownedchatrooms$', OwnedChatroomListCreateView.as_view(), name='owned_chatrooms'),
         url(r'^joinablechatrooms$', JoinableChatroomListView.as_view(), name='joinable_chatrooms'),
+        url(r'^memberchatrooms$', MemberChatroomListView.as_view(), name='member_chatrooms'),
         url(r'^chatroom/(?P<pk>[0-9]+)$', ChatroomDetailView.as_view(), name='chatroom_detail'),
 ]
