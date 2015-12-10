61aa0a229417fd6947f1c6727c0092ef1c208f18
boydjohnson <lt359mn38>
2015-12-10 13:38:04 -0600

    Json that server is giving for last_ten is a string that needs to be seperately deserialized... Will loads so it is an object

--- server.py
+++ server.py
@@ -109,7 +109,7 @@ class YouTubeWebSockets(WebSocketServerProtocol):
                     chatroom_id = id
         if cache.exists(CHATROOM_MESSAGES_KEY(chatroom_id)):
             user_message_dict= cache.lrange(CHATROOM_MESSAGES_KEY(chatroom_id),0, -1)
-            self.sendMessage(dumps({'last_ten': user_message_dict}).encode('utf-8'), isBinary=False)
+            self.sendMessage(dumps({'last_ten': loads(user_message_dict)}).encode('utf-8'), isBinary=False)
         if cache.exists(CHATROOM_SUGGESTIONS_KEY(chatroom_id)):
             user_message_dict = cache.lrange(CHATROOM_SUGGESTIONS_KEY(chatroom_id), 0, -1)
             self.sendMessage(dumps({'suggestion_list': user_message_dict}).encode('utf-8'), isBinary=False)
