b610f33e1e88812926ce3eb20db6aa0f4451504e
boydjohnson <johnson.boyd@gmail.com>
2016-09-12 20:27:35 -0500

    Acting on datamodel, creating ChatMess

--- utubeu/routes.py
+++ utubeu/routes.py
@@ -2,9 +2,11 @@ from channels.routing import route, include
 
 from utubeu_dash.routes import channel_routing as dash_routing
 from utubeu_viewer.routes import channel_route as viewer_routing
+from utubeu_viewer import consumers as con
 
 
 channel_routing = [
     include(dash_routing, path=r'^/ws'),
     include(viewer_routing, path=r'^/c'),
+    route('ChatMess', con.chat_message_consumer)
 ]
--- utubeu_viewer/consumers.py
+++ utubeu_viewer/consumers.py
@@ -1,11 +1,52 @@
 from channels.generic.websockets import JsonWebsocketConsumer
+from channels.channel import Channel, Group
+
+import json
 
 
 class ChatroomConsumer(JsonWebsocketConsumer):
 
+    action_map = {
+        'ChatMess': 'ChatMess',
+        'Sugg': 'Sugg',
+        'VoteSugg': 'VoteSugg',
+        'VotePlaylist': 'VotePlaylist',
+        'VoteCurrent': 'VoteCurrent'
+    }
+
+    def connect(self, message, **kwargs):
+        if 'chatroom' not in kwargs:
+            return
+        Group(kwargs['chatroom'][0:10]).add(self.message.reply_channel)
+
     def receive(self, content, **kwargs):
         if 'chatroom' not in kwargs:
             return
+            # print(self.message.reply_channel)
         if kwargs['chatroom'] != content['chatroom']:
             return
-        print(content)
+        if 'action' not in content:
+            print('No action!')
+            return
+        if content['action'] not in self.action_map:
+            print('NOT in action_map')
+            return
+        results = {k:v for k,v in content.items()}
+        Channel(self.action_map[content['action']]).send(results)
+
+    def disconnect(self, message, **kwargs):
+        print(message.content)
+        if 'chatroom' in kwargs:
+            Group(kwargs['chatroom']).discard(self.message.reply_channel)
+
+
+def chat_message_consumer(message):
+    print(message.content)
+    if 'user' not in message.content:
+        print(message.content)
+        return
+    chatroom = message.content['chatroom']
+    del message.content['chatroom']
+    Group(chatroom[0:10]).send({'text': json.dumps(message.content)})
+
+
