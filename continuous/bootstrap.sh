0dc01c8b00819c81e32cd874c653a41678fb55d3
boydjohnson <johnson.boyd@gmail.com>
2016-10-27 19:38:36 -0500

    Update requirements.txt, add postgres database

--- continuous/bootstrap.sh
+++ continuous/bootstrap.sh
@@ -2,10 +2,10 @@
 
 sudo apt-get -y update
 sudo apt-get -y install redis-server
-sudo apt-get -y install postgresql postgresql-contrib
+sudo apt-get -y install postgresql-9.5 postgresql-contrib-9.5 postgresql-server-dev-9.5
 sudo apt-get -y install python3-pip
 python3 -m pip install --upgrade pip
-cd /utubeu
+cd /vagrant
 python3 -m pip install -r requirements.txt
-echo "alias python=python3" >> ~/.bashrc
+
 
