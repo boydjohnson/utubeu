60146aa70f47ef35a5b1e2512852fc507e1d9a69
yp0975eu(Branden) <yp0975eu@users.noreply.github.com>
2016-11-09 20:53:17 -0600

    broken ajax submit

--- utubeu_viewer/static/assets/js/forms/contact.js
+++ utubeu_viewer/static/assets/js/forms/contact.js
@@ -1,82 +1,90 @@
 var ContactForm = function () {
 
     return {
-        
+
         //Contact Form
         initContactForm: function () {
-	        // Validation
-	        $("#sky-form3").validate({
-	            // Rules for form validation
-	            rules:
-	            {
-	                name:
-	                {
-	                    required: true
-	                },
-	                email:
-	                {
-	                    required: true,
-	                    email: true
-	                },
-	                message:
-	                {
-	                    required: true,
-	                    minlength: 10
-	                },
-	                captcha:
-	                {
-	                    required: true,
-	                    remote: 'assets/plugins/sky-forms-pro/skyforms/captcha/process.php'
-	                }
-	            },
-	                                
-	            // Messages for form validation
-	            messages:
-	            {
-	                name:
-	                {
-	                    required: 'Please enter your name',
-	                },
-	                email:
-	                {
-	                    required: 'Please enter your email address',
-	                    email: 'Please enter a VALID email address'
-	                },
-	                message:
-	                {
-	                    required: 'Please enter your message'
-	                },
-	                captcha:
-	                {
-	                    required: 'Please enter characters',
-	                    remote: 'Correct captcha is required'
-	                }
-	            },
-	                                
-	            // Ajax form submition                  
-	            submitHandler: function(form)
-	            {
-	                $(form).ajaxSubmit(
-	                {
-	                    beforeSend: function()
-	                    {
-	                        $('#sky-form3 button[type="submit"]').attr('disabled', true);
-	                    },
-	                    success: function()
-	                    {
-	                        $("#sky-form3").addClass('submited');
-	                    }
-	                });
-	            },
-	            
-	            // Do not change code below
-	            errorPlacement: function(error, element)
-	            {
-	                error.insertAfter(element.parent());
-	            }
-	        });
+            // Validation
+            $("#createNewChatroomForm").validate({
+                // Rules for form validation
+                rules: {
+                    chatroomName: {
+                        required: true
+
+                    },
+                    chatroomDescription: {
+                        required: true
+                    },
+                    chatroomMaxOccupants: {
+                        required: true,
+                        range: [2, 20]
+
+                    },
+                },
+
+                // Messages for form validation
+                messages: {
+                    chatroomName: {
+                        required: 'Name is required',
+                    },
+                    chatroomDescription: {
+                        required: 'Description is required',
+                    },
+                    chatroomMaxOccupants: {
+                        required: 'Max occupants must be 2-20'
+                    },
+                },
+
+                // Ajax form submition
+                submitHandler: function (form) {
+                    console.log(form);
+                    $("#createNewChatroomForm").submit(function (e) {
+                        e.preventDefault();
+                        $("#createNewChatroomForm").ajaxSubmit(
+                            {
+                                type: "POST",
+                                dataType: "json",
+                                url: "/api/v1/ownedchatrooms",
+                                beforeSend: function () {
+
+                                    //$('form button[type="submit"]').attr('disabled', true);
+
+                                    var name = $("input[name=chatroomName]").val();
+                                    var description = $("input[name=chatroomDescription]").val();
+                                    var isPublic = false;
+                                    var maxOccupants = $("input[name=chatroomMaxOccupants]").val();
+                                    var token = $("input[name=csrfmiddlewaretoken]").val();
+                                    var data = {
+                                        "name": name,
+                                        "description": description,
+                                        "is_public": isPublic,
+                                        "max_occupants": maxOccupants
+                                    };
+
+
+                                },
+                                headers: {
+                                    "X-CSRFToken": token
+                                },
+                                data: data,
+                                success: function () {
+
+                                    $("#createNewChatroomForm").addClass('submited');
+                                }
+
+                            }
+                        );
+                        return false;
+                    });
+                },
+
+                // Do not change code below
+                errorPlacement: function (error, element) {
+                    error.insertBefore(element.parent());
+                }
+            });
         }
 
     };
-    
+
 }();
