871a075870e7b11902ee98ce75c19e1ac8e10702
boydjohnson <johnson.boyd@gmail.com>
2016-11-13 12:36:46 -0600

    Add chatroom querysets to context in dash view

--- utubeu_dash/views.py
+++ utubeu_dash/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
+
+from utubeu_viewer.models import Chatroom
 
 
 def dashboard(request):
     if request.method == 'GET':
         user = request.user
-        return render(request, 'utubeu-dash/dash.html', context={'user': user})
+        member_chatrooms = Chatroom.objects.filter(~Q(owner=user.pk), joiners=user.pk)[:8]
+        owned_chatrooms = Chatroom.objects.filter(owner=user.pk)[:8]
+        public_chatrooms = Chatroom.objects.filter(is_public=True)[:8]
+        return render(request, 'utubeu-dash/dash.html', context={'user': user,
+                                                                 'owned_chatrooms': owned_chatrooms,
+                                                                 'member_chatrooms': member_chatrooms,
+                                                                 'public_chatrooms': public_chatrooms})
     else:
         raise PermissionDenied("Method not supported.")
